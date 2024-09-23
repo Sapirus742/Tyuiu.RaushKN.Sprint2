f7c8c14770dc01b3c78f86cf1e28991f80474065
Кирилл Рауш <Tyuiu.RaushKN@outlook.com>
2024-09-23 21:30:54 +0500

    Sprint2

---
+++ Tyuiu.RaushKN.Sprint2.Task6.V3/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.RaushKN.Sprint2.Task6.V3.Lib;
+
+namespace Tyuiu.RaushKN.Sprint2.Task6.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Рауш К. Н. | АСОиУб-24-1";
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("# Спринт #2                                                               #");
+            Console.WriteLine("# Тема: Получение результата из switch                                    #");
+            Console.WriteLine("# Задание #6                                                              #");
+            Console.WriteLine("# Вариант #3                                                              #");
+            Console.WriteLine("# Выполнил Рауш Кирилл Николавевич | АСОиУб-24-1                          #");
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("# УСЛОВИЕ:                                                                #");
+            Console.WriteLine("# Составить программу, которая в зависимости от порядкового номера дня    #");
+            Console.WriteLine("# недели (1, 2, …, 7) выводит на экран его название (понедельник,         #");
+            Console.WriteLine("# вторник, …, воскресенье).                                               #");
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("# ИСХОДНЫЕ ДАННЫЕ:                                                        #");
+            Console.WriteLine("###########################################################################");
+
+            int value;
+            Console.WriteLine("Введите номер дня: ");
+            value = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+            if ((value < 1) || (value > 7)) res = "Введено неверное значение";
+            else res = "Это месяц: " + ds.FindDayName(value);
+
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("# РЕЗУЛЬТАТ:                                                              #");
+            Console.WriteLine("###########################################################################");
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
