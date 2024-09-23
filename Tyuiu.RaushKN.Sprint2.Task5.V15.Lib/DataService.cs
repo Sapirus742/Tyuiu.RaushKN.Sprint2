55ad76313f602c01b5863d34e7bd53addc7125b3
Кирилл Рауш <Tyuiu.RaushKN@outlook.com>
2024-09-23 20:33:48 +0500

    Sprint2

---
+++ Tyuiu.RaushKN.Sprint2.Task5.V15.Lib/DataService.cs
@@ -0,0 +1,39 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.RaushKN.Sprint2.Task5.V15.Lib
+{
+    public class DataService : ISprint2Task5V15
+    {
+        public string FindDayName(int k)
+        {
+            string res;
+            switch (k)
+            {
+                case 1:
+                    res = "Понедельник";
+                    break;
+                case 2:
+                    res = "Вторник";
+                    break;
+                case 3:
+                    res = "Среда";
+                    break;
+                case 4:
+                    res = "Четверг";
+                    break;
+                case 5:
+                    res = "Пятница";
+                    break;
+                case 6:
+                    res = "Суббота";
+                    break;
+                case 7:
+                    res = "Воскресенье";
+                    break;
+                default:
+                    throw new ArgumentException($"День должен быть от 1 до 7. Значение {k}");
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.RaushKN.Sprint2.Task5.V15/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.RaushKN.Sprint2.Task5.V15.Lib;
+
+namespace Tyuiu.RaushKN.Sprint2.Task5.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Рауш К. Н. | АСОиУб-24-1";
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("# Спринт #2                                                               #");
+            Console.WriteLine("# Тема: Оператор switch                                                   #");
+            Console.WriteLine("# Задание #5                                                              #");
+            Console.WriteLine("# Вариант #15                                                             #");
+            Console.WriteLine("# Выполнил Рауш Кирилл Николавевич | АСОиУб-24-1                          #");
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("# УСЛОВИЕ:                                                                #");
+            Console.WriteLine("# Дано целое число k (1 <= k <= 365). Определить, каким днем недели       #");
+            Console.WriteLine("# (понедельником, вторником, …, субботой или воскресеньем) является k-й   #");
+            Console.WriteLine("# день не високосного года, в котором 1 января понедельник.               #");
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("# ИСХОДНЫЕ ДАННЫЕ:                                                        #");
+            Console.WriteLine("###########################################################################");
+
+            int k;
+            Console.WriteLine("Введите номер дня: ");
+            k = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+            if ((k < 1) || (k > 7)) res = "Введено неверное значение!";
+            else res = "Это день: " + ds.FindDayName(k);
+
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("# РЕЗУЛЬТАТ:                                                              #");
+            Console.WriteLine("###########################################################################");
+        
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
