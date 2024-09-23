0f05a1672f400f22c57926be0a9e552f347871e4
Кирилл Рауш <Tyuiu.RaushKN@outlook.com>
2024-09-23 19:03:34 +0500

    Sprint2

---
+++ Tyuiu.RaushKN.Sprint2.Task3.V2.Test/DataServiceTest.cs
@@ -0,0 +1,45 @@
+using Tyuiu.RaushKN.Sprint2.Task3.V2.Lib;
+
+namespace Tyuiu.RaushKN.Sprint2.Task3.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 4;
+            double res = ds.Calculate(x);
+            double wait = -44.654;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = 1.5;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = -0.577;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -7;
+            double res = ds.Calculate(x);
+            double wait = -77;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
